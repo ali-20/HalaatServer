b114b29e8ede53b8418d83ab9ab84423667037d9
ali-20 <ali.rehan417@gmail.com>
2021-01-24 17:17:59 +0500

    untracked node_modules

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ app.use(bodyparser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(fileUpload());
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
 
