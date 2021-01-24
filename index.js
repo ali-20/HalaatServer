761a80a5cbd2c368cd9389ca4aadc7f326423f6a
ali-20 <ali.rehan417@gmail.com>
2021-01-24 19:01:54 +0500

    added database config

--- index.js
+++ index.js
@@ -12,27 +12,11 @@ const app = express();
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
-var allowedOrigins = [
-  "http://localhost:3000",
-  "https://halaatowaqiat.netlify.app",
-];
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: "https://halaatowaqiat.netlify.app",
   })
 );
-
 app.use(fileUpload());
 
 const PORT = 5000;
