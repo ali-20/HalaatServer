5f8b20231ced23384cba72001194794432912728
ali-20 <ali.rehan417@gmail.com>
2021-01-24 17:30:54 +0500

    added database config

--- connection.js
+++ connection.js
@@ -1,22 +1,19 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-var mysqlconnection= mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password:"password",
-    database: "halaat",
-    multipleStatements: true
+var mysqlconnection = mysql.createConnection({
+  host: "us-cdbr-east-03.cleardb.com",
+  user: "b2db69a34adf99",
+  password: "8f870e1b",
+  database: "heroku_2f1b0ada4311605",
+  multipleStatements: true,
 });
 
-mysqlconnection.connect((err)=>{
-    if(err){
-        console.log(err);
-
-    }
-
-    else {
-        console.log('connection successful');
-    }
+mysqlconnection.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("connection successful");
+  }
 });
 
-module.exports=mysqlconnection;
+module.exports = mysqlconnection;
