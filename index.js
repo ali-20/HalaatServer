9d427296d67e1b8fefdfde570967fbd4c5fa65b0
ali-20 <ali.rehan417@gmail.com>
2021-01-24 21:56:36 +0500

    added database config

--- index.js
+++ index.js
@@ -14,31 +14,9 @@ app.use(bodyparser.urlencoded({ extended: false }));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST");
   next();
 });
 
-var allowedOrigins = [
-  "https://halaatportal.netlify.app",
-  "https://halaatowaqiat.netlify.app",
-  "https://halaatportal.netlify.app/writerlogin",
-];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
 app.use(fileUpload());
 
 const PORT = 5000;
