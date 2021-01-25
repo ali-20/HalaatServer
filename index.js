f43f62e4e152003d843f9c66408836c9838c02ab
ali-20 <ali.rehan417@gmail.com>
2021-01-25 21:34:20 +0500

    added corsheader

--- index.js
+++ index.js
@@ -15,10 +15,26 @@ app.use(bodyparser.urlencoded({ extended: false }));
 
 app.use(fileUpload());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST");
-});
+var allowedOrigins = [
+  "https://halaatowaqiat.netlify.app",
+  "https://halaatportal.netlify.app",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 const PORT = 5000;
 
