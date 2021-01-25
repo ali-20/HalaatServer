263ac654bf45040da144dfca88c2efc166a01dad
ali-20 <ali.rehan417@gmail.com>
2021-01-25 21:30:11 +0500

    added corsheader

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ var cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
 app.use(fileUpload());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST");
+});
 
 const PORT = 5000;
 
