87e4cecc6743aca324be5d358c510c7871aa9773
ali-20 <ali.rehan417@gmail.com>
2021-01-31 17:41:35 +0500

    heroku

--- index.js
+++ index.js
@@ -10,31 +10,37 @@ var cors = require("cors");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
 app.use(fileUpload());
 
-var allowedOrigins = [
-  "https://halaatowaqiat.netlify.app",
-  "https://halaatportal.netlify.app",
-];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// var allowedOrigins = [
+//   "https://halaatowaqiat.netlify.app",
+//   "https://halaatportal.netlify.app",
+// ];
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // allow requests with no origin
+//       // (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         var msg =
+//           "The CORS policy for this site does not " +
+//           "allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 const PORT = 5000;
 
