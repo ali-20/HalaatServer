a2f0e67de4d24b369e262b111a2e682a2596cec4
ali-20 <ali.rehan417@gmail.com>
2021-01-31 17:44:08 +0500

    heroku

--- routes/Scrapped_articleroutes.js
+++ routes/Scrapped_articleroutes.js
@@ -4,19 +4,19 @@ var mangoscrapper = require("./News_Scrapping/News_Scrapper");
 var dawnscrapper = require("./News_Scrapping/Searched_ArticlesScrapped");
 var propakistaniscrapper = require("./News_Scrapping/propakistaniscrapped");
 
-router.post("/mangobaaz", (req, res) => {
+router.get("/mangobaaz", (req, res) => {
   mangoscrapper().then((x) => {
     res.send(x);
   });
 });
 
-router.post("/dawn", (req, res) => {
+router.get("/dawn", (req, res) => {
   dawnscrapper().then((x) => {
     res.send(x);
   });
 });
 
-router.post("/propakistani", (req, res) => {
+router.get("/propakistani", (req, res) => {
   propakistaniscrapper().then((x) => {
     res.send(x);
   });
